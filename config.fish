5a3dce6a8d8eb90a12aa7966d4db035972049e1e
Valentin Brémond <valentin.bremond@gmail.com>
2015-06-25 21:35:48 +0200

    add: start_ssh_agent (replaces ssh-add)

--- config.fish
+++ config.fish
@@ -34,3 +34,25 @@ function _get_git_branch
     echo '['(git rev-parse --abbrev-ref HEAD 2> /dev/null)']'
 end
 
+# Make ssh-add work
+setenv SSH_ENV $HOME/.ssh/environment
+
+function start_agent
+    echo "Initializing new SSH agent..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV
+    . $SSH_ENV > /dev/null
+    ssh-add
+end
+
+function test_identities
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
+    end
+end
+
