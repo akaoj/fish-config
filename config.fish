88a741d0047fb64e56d153e2347c67333f45e6f7
Valentin Bremond <valentin.bremond@scality.com>
2017-05-17 11:04:28 +0200

    Add abbreviations, improve prompt

--- config.fish
+++ config.fish
@@ -6,7 +6,6 @@
 bind \er forward-word
 
 
-
 #################
 # Abbreviations #
 #################
@@ -17,17 +16,20 @@ abbr -a g git
 abbr -a gc git commit -m
 abbr -a gs git status
 abbr -a gb git branch
-abbr -a gu "git checkout master ;and git pull ;and git checkout - ;and git rebase master"
 abbr -a gd git diff
 abbr -a gaa git add --all
-abbr -a gl "git log master.."
+abbr -a gl "git log master.."  # display commits added on top of master
+abbr -a gcd "cd (git rev-parse --show-toplevel)"  # cd in the root folder of the directory
+abbr -a gu "git fetch --tags --prune --all"
 
-abbr -a l "less -SM +Gg"
+abbr -a gv "git diff HEAD~1 --name-only | xargs nvim"  # open all modified files in neovim
 
-abbr -a v vim
+abbr -a l "less -SM +Gg -R"
 
+abbr -a v nvim
+
+abbr -a tf terraform
 
 
 # Make ssh-add work
 setenv SSH_ENV $HOME/.ssh/environment
-
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,15 +1,24 @@
 # Overwrite default prompt
 function fish_prompt
-	set -l _line_color green -o  # Line is bold green
-	set -l _branch_color white   # git branch color is white
+	set -l _last_status (echo $status)
+	set -l _arrow_color green -o  # allow will change its color when the last command failed
+	set -l _line_color green -o   # line is bold green
+	set -l _branch_color white    # git branch color is white
+	set -l _pwd_color 525252      # light grey
 
 	# If root, print in bold red
 	if [ (whoami) = "root" ]
-	    set _line_color red -o
+		set _line_color red -o
 	end
 
-	# Promp looks like:
-	# [user] time workdir [gitBranch] >
-	printf '%s %s %s %s %s%s ' (set_color $_line_color)'['(whoami)'@'(hostname)']' (date '+%H:%M:%S') (pwd) (set_color normal)(set_color $_branch_color)(git_get_branch)(set_color $_line_color) '>'(set_color normal)
-end
+	if [ $_last_status != 0 ]
+		set _arrow_color red -o
+	end
 
+	# Promp looks like (note the blank line before the actual prompt):
+	#
+	#  ┌/path/to/my/current/folder
+	#  └─HH:mm:ss [git branch]>
+	printf '\n\u250C%s\n' (set_color $_pwd_color)(pwd)(set_color normal)
+	printf '\u2514\u2500%s %s%s ' (set_color $_line_color)(date '+%H:%M:%S') (set_color normal)(set_color $_branch_color)(git_get_branch)(set_color $_line_color) (set_color $_arrow_color)'>'(set_color normal)
+end
